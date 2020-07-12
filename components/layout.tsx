2cfd529537f2cc73b9e13fa734166a802501e743
seabass ng <seabass.ng@gmail.com>
2020-07-12 13:20:12 -0700

    Remove the static export. The tabs end up looking too jumpy

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,7 +2,6 @@ import { AppBar, Container } from '@material-ui/core';
 import Head from 'next/head';
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import window from 'global/window';
 import GlobalStyles from '../lib/global-styles';
 import Header from './header';
 import NavTabs from './nav-tabs';
@@ -23,42 +22,34 @@ interface ILayout {
   activeTab?: string;
 }
 
-const Layout: React.FC<ILayout> = ({ activeTab, children }: ILayout) => {
-  const [loaded, setLoaded] = useState(false);
-  useEffect(() => {
-    setLoaded(true);
-  }, []);
-  return (
-    <>
-      <Head key="layout">
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-        />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Norican:400&display=swap"
-        />
-        <meta name="robots" content="noindex" />
-      </Head>
-      <GlobalStyles />
-      <StyledContainer>
-        <Header />
-        {loaded && (
-          <AppBar position="sticky">
-            <NavTabs activeTab={activeTab} />
-          </AppBar>
-        )}
-        <main>{children}</main>
-      </StyledContainer>
-      <StyledFooter>
-        ©{new Date().getFullYear()} <a href="https://github.com/seabass-ng">Sebastian Ng</a> &mdash;
-        Background Image from{' '}
-        <a href="https://pixabay.com/users/mariamichelle-165491/">Michelle Maria</a>
-      </StyledFooter>
-    </>
-  );
-};
+const Layout: React.FC<ILayout> = ({ activeTab, children }: ILayout) => (
+  <>
+    <Head key="layout">
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Norican:400&display=swap"
+      />
+      <meta name="robots" content="noindex" />
+    </Head>
+    <GlobalStyles />
+    <StyledContainer>
+      <Header />
+      <AppBar position="sticky">
+        <NavTabs activeTab={activeTab} />
+      </AppBar>
+      <main>{children}</main>
+    </StyledContainer>
+    <StyledFooter>
+      ©{new Date().getFullYear()} <a href="https://github.com/seabass-ng">Sebastian Ng</a> &mdash;
+      Background Image from{' '}
+      <a href="https://pixabay.com/users/mariamichelle-165491/">Michelle Maria</a>
+    </StyledFooter>
+  </>
+);
 
 Layout.defaultProps = {
   activeTab: null,
